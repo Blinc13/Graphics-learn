9ab9752f674c4634ffd08fe4bc035024a922e075
Blinc <65546512+Blinc13@users.noreply.github.com>
2023-09-09 02:36:43 +0300

    AAB cube rendering

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,8 +1,10 @@
 use std::io::{stdout, Write};
+use std::time::Instant;
+use crossterm::QueueableCommand;
 use graphics_learn::canvas::BasicCanvas;
 use graphics_learn::math::{AxisAlignedBox, Plain};
 use graphics_learn::{Canvas, RayIntersection};
-use graphics_learn::types::{FVec2, FVec3, UVec2};
+use graphics_learn::types::{FVec2, FVec3, FVec4, Mat3, Mat4, UVec2};
 
 fn main() {
     let size = crossterm::terminal::size()
@@ -10,40 +12,57 @@ fn main() {
         .unwrap();
     let mut canvas = BasicCanvas::new(size, FVec3::new(0.1, 0.5, 1.0));
 
+    let mut last_time = Instant::now();
+    let mut total_time = 0.0;
 
-    {
-        let cam_pos = FVec3::new(0.0, 0.0, -3.0);
-        let b = Plain {
-            normal: FVec3::new(0.0, 0.0, 1.0).normalize(),
-            dist: 0.0
-        };
+    loop {
+        {
+            let passed = Instant::now();
+            let dur = passed.duration_since(last_time);
+
+            last_time = passed;
+            total_time += dur.as_secs_f32();
+        }
+
+        let rotation = Mat4::new_rotation(FVec3::new(0.0, 0.0, 0.0));
+        let cam_pos = FVec3::new((total_time / 10.0).sin() * 2.0, 0.0, 1.0);
+        let b = AxisAlignedBox::from_position_and_extents(
+            FVec3::new(1.0, 0.0, 0.0),
+            FVec3::new(0.5, 0.5, 0.5)
+        );
 
         for x in 0..size.x {
             for y in 0..size.y {
                 let pos = UVec2::new(x, y);
-                let render_p = FVec3::new(
+                let render_p = FVec4::new(
                     (x as f32 - size.x as f32 / 2.0) / size.x as f32,
                     (y as f32 - size.y as f32 / 2.0) / size.y as f32,
+                    0.0,
                     0.0
-                ) + FVec3::new(0.0, 0.0, 0.1) + cam_pos;
+                ) + FVec4::new(0.0, 0.0, 0.3, 1.0);
+                let render_p = (rotation * render_p).xyz();
 
-                let dir = (cam_pos - render_p).normalize();
+                let dir = (cam_pos - (render_p + cam_pos)).normalize();
 
                 if let Some((min, max)) = b.intersect(cam_pos, dir) {
                     if min < 0.0 {
-                        continue;
+                       continue;
                     }
 
-                    canvas.put_pixel(pos, FVec3::new(min / 10.0, 0.0, 0.0));
+                    canvas.put_pixel(pos, FVec3::new((max - min), 0.0, 0.0));
                 }
             }
         }
+
+        {
+            let mut out = stdout();
+
+            canvas.write_queued(&mut out).unwrap();
+            out.queue(crossterm::cursor::MoveTo(0, 0)).unwrap();
+
+            out.flush().unwrap();
+        }
+
+        canvas.fill(FVec3::new(0.1, 0.5, 1.0));
     }
-
-
-    let mut out = stdout();
-
-    canvas.write_queued(&mut out).unwrap();
-
-    out.flush().unwrap();
 }
--- src/math.rs
+++ src/math.rs
@@ -1,7 +1,6 @@
-use std::ops::Range;
-use nalgebra::RawStorage;
+use smallvec::SmallVec;
+use crate::types::FVec3;
 use crate::RayIntersection;
-use crate::types::{FVec2, FVec3};
 
 #[derive(Debug, Copy, Clone)]
 pub struct Plain {
@@ -15,7 +14,7 @@ impl Plain {
     pub fn invert(self) -> Self {
         Self {
             normal: -self.normal,
-            dist: -self.dist
+            dist: self.dist
         }
     }
 }
@@ -29,7 +28,7 @@ impl RayIntersection for Plain {
             return None
         }
         let dot = self.normal.dot(
-            &(origin - self.normal * self.dist)
+            &(self.normal * self.dist - origin)
         );
         let res = dot / dot_dir;
 
@@ -38,58 +37,40 @@ impl RayIntersection for Plain {
 }
 
 #[derive(Debug, Copy, Clone)]
-pub enum AABFace {
-    XPos,
-    XNeg,
-    YPos,
-    YNeg,
-    ZPos,
-    ZNeg
-}
-
-impl Into<FVec3> for AABFace {
-    #[inline(always)]
-    fn into(self) -> FVec3 {
-        match self {
-            AABFace::XPos => FVec3::new(1.0, 0.0, 0.0),
-            AABFace::XNeg => FVec3::new(-1.0, 0.0, 0.0),
-            AABFace::YPos => FVec3::new(0.0, 1.0, 0.0),
-            AABFace::YNeg => FVec3::new(0.0, -1.0, 0.0),
-            AABFace::ZPos => FVec3::new(0.0, 0.0, 1.0),
-            AABFace::ZNeg => FVec3::new(0.0, 0.0, -1.0)
-        }
-    }
-}
-
 pub struct AxisAlignedBox {
     pub pos: FVec3,
     pub extents: FVec3
 }
 
 impl AxisAlignedBox {
-    // Assumes what int was divided by extents
     #[inline(always)]
-    fn check_face_intersection(face: AABFace, int: FVec3) -> bool {
-        const RANGE: Range<f32> = -1.0..1.0;
-
-        let check_edges = match face {
-            AABFace::XPos => FVec2::new(int.y, int.z),
-            AABFace::XNeg => FVec2::new(int.y, int.z),
-            AABFace::YPos => FVec2::new(int.x, int.z),
-            AABFace::YNeg => FVec2::new(int.x, int.z),
-            AABFace::ZPos => FVec2::new(int.x, int.y),
-            AABFace::ZNeg => FVec2::new(int.x, int.y)
-        };
-
-        let b = RANGE.contains(&check_edges.x) && RANGE.contains(&check_edges.y);
-
-        if b {
-            println!("Intersects!");
-        } else {
-            println!("Not intersect!");
+    pub fn from_position_and_extents(pos: FVec3, extents: FVec3) -> Self {
+        Self {
+            pos,
+            extents
         }
+    }
 
-        b
+    #[inline(always)]
+    pub fn translate(&mut self, by: FVec3) {
+        self.pos += by;
+    }
+
+    #[inline(always)]
+    pub fn scale(&mut self, by: FVec3) {
+        self.extents = self.extents.component_mul(&by);
+    }
+
+    #[inline(always)]
+    pub fn contains(&self, point: FVec3) -> bool {
+        self.contains_local(self.pos - point)
+    }
+
+    #[inline(always)]
+    fn contains_local(&self, point: FVec3) -> bool {
+        let point = point.abs();
+
+        point.x <= self.extents.x && point.y <= self.extents.y && point.z <= self.extents.z
     }
 }
 
@@ -97,54 +78,35 @@ impl RayIntersection for AxisAlignedBox {
     fn intersect(&self, origin: FVec3, direction: FVec3) -> Option<(f32, f32)> {
         let origin = origin - self.pos;
 
-        let plain_types = [
-            AABFace::XPos,
-            AABFace::XNeg,
-            AABFace::YPos,
-            AABFace::YNeg,
-            AABFace::ZPos,
-            AABFace::ZNeg
-        ];
-        let plains = [
-            Plain {
-                normal: FVec3::new(1.0, 0.0, 0.0),
-                dist: self.extents.x
-            },
-            Plain {
-                normal: FVec3::new(-1.0, 0.0, 0.0),
-                dist: -self.extents.x
-            },
-            Plain {
-                normal: FVec3::new(0.0, 1.0, 0.0),
-                dist: self.extents.y
-            },
-            Plain {
-                normal: FVec3::new(0.0, -1.0, 0.0),
-                dist: -self.extents.y
-            },
-            Plain {
-                normal: FVec3::new(0.0, 0.0, 1.0),
-                dist: self.extents.z
-            },
-            Plain {
-                normal: FVec3::new(0.0, 0.0, -1.0),
-                dist: -self.extents.z
-            }
+        const NORMALS: &[FVec3] = &[
+            FVec3::new(-1.0, 0.0, 0.0),
+            FVec3::new(0.0, -1.0, 0.0),
+            FVec3::new(0.0, 0.0, -1.0),
+
+            FVec3::new(1.0, 0.0, 0.0),
+            FVec3::new(0.0, 1.0, 0.0),
+            FVec3::new(0.0, 0.0, 1.0)
         ];
 
-        let mut iter = plains.iter().copied()
-            .zip(plain_types.iter().copied())
-            .map(| (plain, plain_type) | (plain.intersect(origin, direction).map(| r | r.0), plain, plain_type))
-            .filter_map(| (int, plain, plaint_type) | int.and_then(| int | Some((int, plain, plaint_type))))
-            .map(| (int, plain, plain_type) | (origin + direction * int, int, plain, plain_type))
-            .filter(| (int, _, _, plain_type) | AxisAlignedBox::check_face_intersection(*plain_type, *int))
-            .map(| (_, int, _, _) | int);
+        let components_iter = self.extents.iter()
+            .copied()
+            .chain(self.extents.iter().copied());
 
-        Some (
-            (
-                iter.next()?,
-                iter.next()?,
-                )
-        )
+        let mut ints: SmallVec<[f32; 2]> = NORMALS
+            .iter()
+            .copied()
+            .zip(components_iter)
+            .filter_map(| (n, d) | Plain {normal: n, dist: d}.intersect(origin, direction))
+            .map(| (d, _) | (d, origin + direction * d))
+            .filter(| (_, p) | self.contains_local(*p * 0.99))
+            .map(| (d, _) | d).collect();
+
+        if ints.len() < 2 {
+            None
+        } else {
+            ints.sort_by(| a, b | a.total_cmp(b));
+
+            Some((ints[0], ints[1]))
+        }
     }
 }
